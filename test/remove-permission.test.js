687e9dbd2d8b3bbfa90f5a525c28f6c5c2b84815
James Mortemore <james.mortemore@clock.co.uk>
2015-03-19 23:42:09 +0000

    Improve coverage

--- test/remove-permission.test.js
+++ test/remove-permission.test.js
@@ -3,13 +3,22 @@ var assert = require('assert')
 
 describe('#removePermission', function () {
 
-  it('should throw an error if group does not have permission', function () {
+  it('should throw an error if resource does not have permission', function () {
     var fellowship = new Fellowship()
 
     fellowship.addResource('test', [ 'hello', 'world' ])
     fellowship.addGroup('test', { test: 2 })
 
     assert.throws(fellowship.removePermission.bind(fellowship, 'test', 'test', 'a'), /Permission a does not exist in test/)
+  })
+
+  it('should throw an error if group does not have permission', function () {
+    var fellowship = new Fellowship()
+
+    fellowship.addResource('test', [ 'hello', 'world' ])
+    fellowship.addGroup('test', { test: 2 })
+
+    assert.throws(fellowship.removePermission.bind(fellowship, 'test', 'test', 'hello'), /Permission hello not defined within test/)
   })
 
   it('should successfully remove a permission', function () {
